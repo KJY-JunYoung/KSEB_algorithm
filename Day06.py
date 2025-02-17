66bdab0a1f68f16f0b90106deaf09a443bcb65db
KJY <12181467@inha.edu>
2025-02-17 09:50:43 +0900

    v0.3 day06 new bit operation

--- Day06.py
+++ Day06.py
@@ -1,17 +1,8 @@
 def is_even(n) -> bool:
 
+    return not n & 1
 
-
-
-    if n % 2 == 0:
-        return True
-    return false
 
 n= int (input())
 print(is_even(n))
 
-a = 10
-b = 11
-print(a & b)
-print(a | b)
-print(a ^ b)
