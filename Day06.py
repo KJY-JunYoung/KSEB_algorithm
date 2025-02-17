e3ca6101aeaf5ae28fe2bc23d3e8decbc69cccef
KJY <12181467@inha.edu>
2025-02-17 16:54:56 +0900

    v1.0 day06 name random choice program

--- Day06.py
+++ Day06.py
@@ -1,13 +1,22 @@
 import random
-students= []
 
 try:
-    file = input("File name : ")
-    fp = open(file, 'r')
-    readme_list = fp.readlines()
-    rls = readme_list[0].split(' ')
-    print(readme_list)
-    print(rls)
-    fp.close()
+    with open("student.csv", 'r') as fp:
+        students_list = fp.readlines()
+        students_list.remove("***\n") # 특정 이름 제거
+        students_list.remove("***\n")
+        students_list.remove("***\n")
+        students_list.remove("***\n")
+        students_list.remove("***\n")
+
+        for _ in range(3):
+            random_pick = random.choice(students_list)
+            print(random_pick, end='')
+            students_list.remove(random_pick)
+
+        # if "***\n" in students_list:
+        #     print("!")
+        # print(students_list[1], end='')
+
 except FileNotFoundError as err:
-    print(f"{file} is not exist. {err}")
+    print(err)
