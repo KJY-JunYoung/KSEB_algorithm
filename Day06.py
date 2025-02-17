b27b84c81d8d6adfb32385acab44ef612e1cc819
KJY <12181467@inha.edu>
2025-02-17 10:41:45 +0900

    v0.4  convert decimal to octal

--- Day06.py
+++ Day06.py
@@ -1,8 +1,23 @@
 def is_even(n) -> bool:
 
+    """
+    짝수 판정 함수
+    :param n: 판정할 정수
+    :return: 짝수면 True, 홀수면 False
+    """
+
     return not n & 1
+
+def DectoOcto(n) -> int:
+
+    if n == 0:
+        return ""
+    else:
+        return DectoOcto(n // 8) + str(n % 8)
+
 
 
 n= int (input())
-print(is_even(n))
+print(DectoOcto(n))
+
 
