99427a28e256791e53eb31c7e8e52622cdfcc72f
KJY <12181467@inha.edu>
2025-02-17 16:30:37 +0900

    v0.9 day06 python file import

--- Day06.py
+++ Day06.py
@@ -1,11 +1,13 @@
-import numpy as np
+import random
+students= []
 
-narray = np.array([1, 3.1, 2, 9])
-print(type(narray))
-print(narray[0], narray[1], narray[2], narray[3])
-print(type(narray), type(narray[2]), type(narray[3]))
-
-array = [9, -11, '8', 7]
-print(array[0], array[1], array[2], array[3])
-print(type(array), type(array[2]), type(array[3]))
-print(id(array[0]),id(array[1]),id(array[2]),id(array[3]))
+try:
+    file = input("File name : ")
+    fp = open(file, 'r')
+    readme_list = fp.readlines()
+    rls = readme_list[0].split(' ')
+    print(readme_list)
+    print(rls)
+    fp.close()
+except FileNotFoundError as err:
+    print(f"{file} is not exist. {err}")
