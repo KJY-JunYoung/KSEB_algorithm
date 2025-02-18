a7cd31c34e02193555771dd4cffd8aafb935deb0
KJY <12181467@inha.edu>
2025-02-18 11:25:06 +0900

    v1.9 about deque

--- Day07.py
+++ Day07.py
@@ -1,33 +1,8 @@
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
+from collections import deque
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+d = deque()
+d.append('Harry')
+d.append('Potter')
 
-    def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = Node(data)
-
-    def __str__(self):
-        node = self.head
-        while node is not None:
-            print(node.data)
-            node = node.next
-        return "end"
-
-
-
-if __name__ == "__main__":
-    l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    print(l)
+for item in d:
+    print(item)
