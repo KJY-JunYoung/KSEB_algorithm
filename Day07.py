75c23072a1717ac5253df4f53bf12602a03ac599
KJY <12181467@inha.edu>
2025-02-18 11:02:44 +0900

    v1.8 example of linked list

--- Day07.py
+++ Day07.py
@@ -1,15 +1,23 @@
-def a(i):
-    j = 9
-    print(i,j)
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
-def b(k):
-    a(1)
-    print(k)
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-def main():
-    print("start")
-    b(3)
-    print("end")
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(data)
 
 if __name__ == "__main__":
-    main()
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
