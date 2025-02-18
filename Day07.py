a9102aafbd2aee8933e99b98bd07d218a96995b0
KJY <12181467@inha.edu>
2025-02-18 11:31:40 +0900

    v2.0 Day07 linked list how to search

--- Day07.py
+++ Day07.py
@@ -1,8 +1,44 @@
-from collections import deque
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
-d = deque()
-d.append('Harry')
-d.append('Potter')
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-for item in d:
-    print(item)
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(data)
+
+    def search(self, target):
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
+
+    def __str__(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node = node.next
+        return "end"
+
+
+
+if __name__ == "__main__":
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
+    print(l)
+    print(l.search(-11))
+    print(l.search(999))
