588acadea39d99993ec31e6ed3c90bc842b58afd
KJY <12181467@inha.edu>
2025-02-19 14:55:04 +0900

    day08.py add

--- Day07.py
+++ Day07.py
@@ -1,44 +1,26 @@
 
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
 class Queue:
     def __init__(self):
-        self.front = None
-        self.rear = None
-        self._size = 0
+        self.s1 = []
+        self.s2 = []
 
     def enqueue(self, data):
-        self._size = self._size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.next = node
-            self.rear = node     # increase rear
+        while len(self.s1) != 0:
+            self.s2.append(self.s1.pop())
+        self.s1.append(data)
+        while len(self.s2) != 0:
+            self.s1.append(self.s2.pop())
 
     def dequeue(self):
-        if self.front is None:
+        if len(self.s1) == 0:
             raise IndexError('dequeue from empty queue')
-        self._size = self._size - 1
-        temp = self.front # back up
-        self.front = self.front.next
-        if self.front is None:
-            self.rear = None
-        return temp.data
-
-    def size(self):
-        return self._size
+        return self.s1.pop()
 
 if __name__ == "__main__":
     q = Queue()
     q.enqueue(7)
     q.enqueue(-11)
     q.enqueue(8)
-    print(q.size())
-    for _ in range(q.size()):
+    for _ in range(3):
         print(q.dequeue())
-    print(q.size())
+
